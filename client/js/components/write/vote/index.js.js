a25408a3c210ddd26f8b6b966616786a8114eab2
Ron Vaisman <vaismanron@gmail.com>
2015-06-20 01:51:26 -0400

    Added component files

---
+++ client/js/components/write/vote/index.js.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Radium from 'radium';
+
+export default React.createClass ({  
+  getInitialState() {
+    return {}
+  },
+  render() {
+    return <div>
+   
+    </div>
+  }
+})
